612478012ea688e66831d8fdef8cf05c0d9749e2
Narendra yadav <kumarnarendra75524@gmail.com>
2023-06-01 00:06:19 +0530

    Intial File Setup

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=7" />
+
+    <link
+      rel="shortcut icon"
+      href="./assets/logo and favicon/log.jpg"
+      type="image/x-icon"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+    />
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    />
+
+    <link rel="stylesheet" href="./css/navbar.css" />
+
+    <link rel="stylesheet" href="./css/index.css" />
+
+    <link rel="stylesheet" href="./css/footer.css" />
+
+    <title>Poornima Institute Of Engineering And Technology</title>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #1545b3 !important;">
+      <a class="navbar-brand" href="./index.html">
+        <img
+          class="logo-one"
+          src="./assets/logo and favicon/logg.png"
+          alt="Navbar logo"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="true"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.html">Home</a>
+          </li>
+
+         
+
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About Us</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./service-pages/result-form.html"
+              >Result</a
+            >
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link btn btn-primary"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span>Login Here</span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="./service-pages/admin-login.html"
+                >As Admin</a
+              >
+              <a class="dropdown-item" href="./php/logout.php"
+                >Logout</a
+              >
+
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div style="padding: 3.4rem;" class="space"></div>
+
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="./assets/images/slider3.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/images/clg.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/images/slider.png" alt="Third slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/images/slider4.jpg" alt="Fourth slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+
+    <section id="about">
+        <h1 class="heading">About Us</h1>
+
+        <h5 class="about-text text-center">
+            PIET College, jaipur (formerly known as Poornima Institute of Engineering and Technology) was established in 2007 with the aim of imparting pragmatic technical education. In its magnificent journey of 12 years, PIET has set benchmarks and reached at new pinnacles in Engineering Education with dedication, perseverance and devotion. PIET is spearheading its outstanding voyage with the motto ‘Success is not a destination, it’s a journey’
+            <li>
+              State of Art of Infrastructure for innovative Teaching Pedagogy and ICT based learning.
+            </li>
+            <li>Offering five streams of Engineering (CE, ECE, EE,ME & Civil) at UG level.</li>
+            <li> Ranked 4th by Rajasthan Technical University under Quality Index Value Framework</li>
+            <li> Accredited with NBA for B.Tech CSE & B.Tech EE in 2018</li>
+            <li>Collaboration with IBM for research on Business Intelligence and Cloud Computing.</li>
+            <li>Research Grants of more than 30 Lac in 5 years</li>
+            <li>Faculty members getting recognised at National & International level</li>
+            <li>Several projects sponsored by AICTE & DST, Govt. of Rajasthan.</li>
+            <li>IEEE, ISTE, IE (I) and ISLE Student chapters.</li>
+            <li>Arbuda Convention Center: A Multimedia Auditorium with 500+ seating capacity.</li>
+            <li>Placement oriented initiatives for skill development and Outcome of quality placements</li>
+            <li>Catering to diverse category of students from all regions of nation</li>
+            <li>Catering to diverse category of students from all regions of nation</li>
+            <li>Quality hostel & accommodation facility</li>
+            <li>Hands on Learning with Project Oriented Lab & Non Syllabus Projects</li>
+          </h5>
+       
+    </section>
+
+    <h1 class="heading">Our Association</h1>
+
+    <section class="assocaition">
+        <div class="container">
+            <div class="row">
+                <div class="clss">
+                    <img  src="./assets/association/PIETNBABanner.jpg" alt="association-1">
+               </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="parallax1">
+      <h1 class="cl">
+       <center>Our Placement and Companies</center> 
+      </h1>
+        
+      <h5 class="cls">
+        <img src="./assets/images/cmp.png"
+        </h5>
+    </div>
+
+    <h1 class="heading">TESTIMONIALS</h1>
+
+    <section id="testimonials">
+      <div id="testimonial-carousel" class="carousel slide" data-ride="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active container-fluid">
+            <img class="testimonial-img" src="./assets/logo and favicon/yogendra.png" alt="Julen">
+            <h4 class="testimonial-name">Yogendra Singh</h4>
+            <h5>
+                Alumni, Batch 2013-17 (BSF,Sub Inspector)<br>
+                UBS
+            </h5>
+            <h5 class="testimonial-text text-center">
+              Poornima Group has been the opportunity of a lifetime and
+              has meant so much to all of us. I will say Thank You Poornima Group for building the
+              right character and the cognitive skills that I need to keep expanding. I will treasure
+              my memories with Poornima group for the rest of my life.
+            </h5>
+          </div>
+          <div class="carousel-item container-fluid">
+            <img class="testimonial-img" src="./assets/logo and favicon/ruchi.png" alt="Aparup">
+            <h4 class="testimonial-name">Ruchi Pareek</h4>
+            <h5>
+                Alumni, 2009-13 (Cofounder, smalt and beryl pvt.ltd)
+            </h5>
+            <h5 class="testimonial-text text-center">
+              I just want to Thank Poornima Group for everything it has
+              trained me for. Your encouragement and support has helped me in creating a niche for
+              myself at workplace. I am highly grateful for instilling in me the spirit of
+              entrepreneurship. It has an incubation cell that promotes its students to think
+              innovatively and come up with new ideas related to startups. Certainly, Poornima is an
+              exceptional institution and I miss my days spent here.
+            </h5>
+          </div>
+          <div class="carousel-item container-fluid">
+            <img class="testimonial-img" src="./assets/logo and favicon/sharma.png" alt="Abinash">
+            <h4 class="testimonial-name">Shivani Sharma</h4>
+            <h3>
+                Alumni, Batch 2010-14 (RAS)
+            </h3>
+            <h5 class="testimonial-text text-center">
+              Poornima empowered me and instilled in me the
+              sportsmanship required to build a team and work as an effective team player. I truly
+              appreciate the opportunity it bestowed on me by allowing me to participate in various
+              sports and cultural activities conducted by the Clubs.
+            </h5>
+          </div>
+          <div class="carousel-item container-fluid">
+            <img class="testimonial-img" src="./assets/logo and favicon/bhagraj.png" alt="Ramyasri">
+            <h4 class="testimonial-name">BhagRaj Choudhary</h4>
+            <h3>
+                Alumni, Btach 2009-13 (Weatherford International)
+            </h3>
+            <h5 class="testimonial-text text-center">
+               Thank You to every person who has helped, supported, and
+                guided me throughout my tenure. My colleagues and professors have offered me words of
+                advise, love, and encouragement. All of which have propelled me forward to this point,
+                where I would not be, without their guidance. I will forever hold each of you and the
+                memories of Poornima Group.
+            </h5>
+          </div>
+          <a class="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a class="carousel-control-next" href="#testimonial-carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
+        </div>
+    </section>
+
+    <footer id="contact">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <a href="./index.html">
+                <img
+                  class="logo2"
+                  src="./assets/logo and favicon/log.jpg"
+                  alt="logo2"
+                />
+              </a>
+              <p class="content1">
+                Poornima Institute of Engineering & Technology, was established in 2007 with the aim of imparting pragmatic technical education. In its magnificent journey of 12 years, PIET has set benchmarks and reached at new pinnacles in Engineering Education with dedication, perseverance and devotion. PIET is spearheading its outstanding voyage with the motto ‘Success is not a destination, it’s a journey’.
+                 <br /><br />
+                 Our vision is to be a renowned and Globally Recognized College giving importance to academic excellence with latest technology, Research, Innovation and Entrepreneurial Attitude.
+              </p>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <h6 class="content2">CONTACT US</h6>
+              <div class="social-icons">
+                <a
+                  class="icon-link"
+                  href="https://www.facebook.com/PoornimaInstitute/"
+                  ><i class="fa fa-facebook" aria-hidden="true"></i
+                ></a>
+                <a class="icon-link" href="https://www.instagram.com/piet_jaipur/"
+                  ><i class="fa fa-instagram" aria-hidden="true"></i
+                ></a>
+                <a class="icon-link" href="https://twitter.com/PIET_Jaipur?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+                  ><i class="fa fa-twitter" aria-hidden="true"></i
+                ></a>
+                <a
+                  class="icon-link"
+                  href="https://in.linkedin.com/school/poornima-group-of-colleges/"
+                  ><i class="fa fa-linkedin" aria-hidden="true"></i
+                ></a>
+                <a
+                  class="icon-link"
+                  href="https://www.youtube.com/watch?v=fKDwKqlpR3M&pp=ygUucG9vcmltYSBpbnN0aXR1ZSBvZiBlbmdpbmVlcmluZyBhbmQgdGVjaG5vbG9neQ%3D%3D"
+                  ><i class="fa fa-youtube" aria-hidden="true"></i
+                ></a>
+                <a
+                  class="icon-link"
+                  href="https://api.whatsapp.com/send?phone=99286 66222 &amp;text=I%20want%20to%20know%20more%20about%20PIET College%20%20%20"
+                  ><i class="fa fa-whatsapp" aria-hidden="true"></i
+                ></a>
+
+
+                <br /><br />
+                <p class="contact-details">
+                  <i class="fa fa-envelope" aria-hidden="true"></i> &nbsp;
+                  <a href="info@poornima.org" style="color:#fff; text-decoration: none;">info@poornima.org</a>
+                  <br /><br />
+                  <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+                  +91 9928555222, 9928666222 <br /><br />
+                  <i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp;
+                  ISI-2, RIICO Institutional Area, Goner Road, Sitapura,
+                  Jaipur - 302022
+                </p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 mt-3">
+              <h4>PIET College, Jaipur</h4>
+              <div>
+                <iframe src=""></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="footer2">
+          PIET College&nbsp;&copy;&nbsp;2023 All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="./javascript/index.js"></script>
+   
+
+  </body>
+</html>
